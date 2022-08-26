2d1fa3e6f2b71dea1e2bae4ac2c34cf721019f24
maurice <maurice.arida66@gmail.com>
2022-08-26 16:21:02 +0300

    snow

---
+++ src/components/Story.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../index.css";
+
+const Story = () => {
+  return (
+    <div className="story-main d-flex">
+      <div className="">
+        <h2>
+          The Story <br /> of Solim
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet. Ut repellendus iusto aut nihil eligendaut
+          dolor expedita.
+        </p>
+        <h6>
+          Lorem ipsum dolor sit amet. Ut repellendus iusto aut nihil eligendaut
+          dolor expedita. Lorem ipsum dolor sit amet. Ut repellendus iusto aut
+          nihil eligendaut .dolor expedita. Lorem ipsum dolor sit amet. Ut
+          repellendus iusto aut nihil eligendaut .dolor expedita.
+        </h6>
+        <h6>
+          Lorem ipsum dolor sit amet. Ut repellendus iusto aut nihil eligendaut
+          dolor expedita. Lorem ipsum dolor sit amet. Ut repellendus iusto aut
+          nihil eligendaut .dolor expedita. Lorem ipsum dolor sit amet.
+        </h6>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Story;
