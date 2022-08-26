56e5b09c1d2e2d494af68ae606f5279907518ddc
maurice <maurice.arida66@gmail.com>
2022-08-26 16:59:14 +0300

    sodvnos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 import Story from "./components/Story";
+import Roadmap from "./components/Roadmap";
+import Team from "./components/Team";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Story />
+      <Roadmap />
+      <Team />
+      <Footer />
     </div>
   );
 }
